7111364aba5e44ae5612d924e558c85583d77d08
rob <a@b.com>
2019-06-11 07:13:04 -0700

    can count and provide a percent per charater

--- monocrack.py
+++ monocrack.py
@@ -1,3 +1,18 @@
 #!/usr/ibn/python
 
+import string
+import collections
+
+transalpha = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+
+scoretextfile = open('score.txt', 'r') #score.txt is the file the program will score and find cribs
+scoreciphertext = scoretextfile.read()
+alphabet = list(string.ascii_uppercase)
+score = collections.Counter(scoreciphertext)
+scoresum = sum(score.values())
+sortedscore= sorted(score, key=score.get, reverse=True)
+
+for char in sortedscore:
+    print(char, score[char],'\t{0:.0%}'.format(score[char]/scoresum))
+
 
