cd0d91e069bb5054bc57cfb44a2f744a4bcbfbdd
rob <a@b.com>
2019-06-12 07:55:09 -0700

    more progress towards basic functionality. i have a was to find double letter cribs and i can translate charater like tr. but stuck on charswap and display

--- monocrack.py
+++ monocrack.py
@@ -2,8 +2,8 @@
 
 import string
 import collections
-
-transalpha = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+import signal
+import re
 
 scoretextfile = open('score.txt', 'r') #score.txt is the file the program will score and find cribs
 scoreciphertext = scoretextfile.read()
@@ -13,6 +13,37 @@ scoresum = sum(score.values())
 sortedscore= sorted(score, key=score.get, reverse=True)
 
 for char in sortedscore:
-    print(char, score[char],'\t{0:.0%}'.format(score[char]/scoresum))
+    print(char, '{0:.0%}'.format(score[char]/scoresum))
 
+transalpha = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+scorealpha = ''.join(sortedscore)
 
+#print(scoreciphertext.translate(str.maketrans(scorealpha, transalpha)))
+
+def doublelettercrib(text):
+    cribs = re.findall(r"(\w)\1+", text)
+    print(cribs)
+
+def alphatrans(score, trans, text):
+    print("before:\t", score)
+    print("after:\t", trans)
+    newtext = scoreciphertext.translate(str.maketrans(score, trans))
+    return newtext
+
+def charswap(first, secound, string):
+    stringlist = list(string)
+    stringlist[first], stringlist[secound] = stringlist[secound], stringlist[first]
+    return ''.join(stringlist)
+
+newscoretext = ''
+c = 1
+print(scoreciphertext)
+doublelettercrib(scoreciphertext)
+while c == 1:
+    num1 = int(input("Enter position for first charater to swap: "))
+    num2 = int(input("Enter secound: "))
+    transalpha = charswap(num1-1, num2-1, transalpha)
+    newscoretext = alphatrans(scorealpha, transalpha, newscoretext)
+
+if __name__ == '__main__':
+    
